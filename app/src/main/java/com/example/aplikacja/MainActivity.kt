fa02412754656c0938134804dc5b1c747be35a3b
ogkamilek <kamil.kozlowski.hel@gmail.com>
2022-10-24 14:48:54 +0200

    Button

---
+++ app/src/main/java/com/example/aplikacja/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.aplikacja
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //obsluga klikniecia
+        findViewById<Button>(R.id.przycisk).setOnClickListener {
+            findViewById<Button>(R.id.przycisk).text="dziala"
+        }
+        findViewById<Button>(R.id.cecha).setOnClickListener {
+            findViewById<Button>(R.id.cecha).textSize=
+        }
+
+    }
+}
